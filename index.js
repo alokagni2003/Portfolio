d5356ff3e2ce44dc4b06d06c0c2fab4b2ffd16ee
Alok Agnihotri <100911488+alokagni2003@users.noreply.github.com>
2023-12-10 02:15:39 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+// let arr=[10,20,30]
+//! splice() method
+//?insertion
+// arr.splice(3,0,100,200,300)
+
+//!slice method
+// let copied_array=arr.slice(0,2);
+// console.log(arr)
+// console.log(copied_array)
+
+//!indexof method
+
+let arr=[10,20,30,40,50,10,300,400]
+// console.log(arr.indexOf(10))
+console.log(arr.reverse)
